b24b91453972cbbbf65d46d32efc5cfcebc7ab4f
Miloš Stojanović <85874608+milos-stojanovic98@users.noreply.github.com>
2021-11-11 19:35:05 +0000

    Added comments, removed trash code. Client changed to Discord.Client

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const Discord = require("discord.js");
 const { Client, Intents } = require("discord.js");
 const keepAlive = require("./server");
 const fetch = require("node-fetch");
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],});
 
+//Bot prefix
 const prefix = "$";
 
+//.env secrets
+const GUILD_ID = process.env['guild_id'];
+const CLIENT_ID = process.env['client_id'];
+
+//Zen quotes function + API
 function getQuote() {
   return fetch("https://zenquotes.io/api/random")
     .then(res => {
@@ -13,46 +20,30 @@ function getQuote() {
     .then(data => {
       return data[0]["q"] + " - " + data[0]["a"];
     })
-
 }
-
-const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-});
+//Message on successfully started app
 client.once("ready", () => {
   console.log("Hasbulla vas sada posmatra !!");
 })
 
-//Komentar
-// ProbaTest2
-
+//Commands
 client.on("messageCreate", (msg) => {
+  //Bot will not react to prefixless messages
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
   const args = msg.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (command === "seva") {
-    msg.reply("Cigan!");
-  } else if (command === "seha") {
-    msg.reply("Cigan najveci!");
-  } else if (msg.content === "$inspire") {
+  if (msg.content === "$inspire") {
     getQuote().then(quote => msg.reply(quote))
-  } else if (command === "micic") {
-    msg.reply("Moj brat, gospodin! GOSPODAR VREMENA");
-  } else if (command === "milorad") {
-    msg.reply("Najveci jebac :D ");
-  } else if (command === "kale") {
-    msg.reply("Mmmmmmmbraleeeeeeeee!");
   } else if (command === "proba") {
     msg.reply("Radiiiiiiii!");
-  } else if (command === "pokusaj") {
-    msg.reply("pokusaj uspesan!");
   }
-
 })
 
+//Function for 24/7 bot hosting
 keepAlive();
 
+//.env secrets (bot token)
 const token = process.env['token']
 client.login(token);
